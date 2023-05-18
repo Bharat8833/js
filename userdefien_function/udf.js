b8bb441a7998e5b08b4fd576507c711032680512
Bharat8833 <bharatdhola456@gmail.com>
2023-05-18 11:41:13 +0530

    ;cfdjmw

--- userdefien_function/udf.js
+++ userdefien_function/udf.js
@@ -1,7 +1,30 @@
-function add(){
-    let a =promt("enter a");
-    let b =promt("enter b");
-    c=a+b;
-    document.write("additin is :"+c);
+//tack nothing return nothing
+function add(){   //declair function
+    let a =prompt("enter a");
+    let b =prompt("enter b");
+    c=parseInt(a)+parseInt(b);
+    document.write("additin is :"+c+"<br>");
+    mult(a,b);
+    document.write('<br>substraction is :'+sub());
 }
-add();
+
+// add(); //call function
+
+//tack nothing return somthing
+
+function sub(){
+    let a =prompt("enter a");
+    let b =prompt("enter b");
+    c=parseInt(a)-parseInt(b);
+    return c;
+}
+// document.write('<br>substraction is :'+sub());
+
+
+//tack somthing return nothing
+
+function mult(a,b){
+  
+    c=parseInt(a)*parseInt(b);
+    document.write("multiplication  is :"+c);
+}

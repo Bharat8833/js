8ef364965a939d33c03a1cdefc9cf4af5401b011
Bharat8833 <bharatdhola456@gmail.com>
2023-06-01 13:06:21 +0530

    gbfn

--- calculator/index.js
+++ calculator/index.js
@@ -1,6 +1,13 @@
-let text;
+let text='';
 
 function appendkey(digit){
     text += digit;
     document.getElementById('screen').value = text;
+}
+
+function equal(){
+    var b=document.getElementById('screen').value;
+    b=b.replace(/x/g,"*");
+    var c=eval(b);
+    document.getElementById('screen').value =c;
 }

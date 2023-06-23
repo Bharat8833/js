94b10cad847e2b0aa5eb91fa7f114d660c763e9e
Bharat8833 <bharatdhola456@gmail.com>
2023-06-23 11:43:54 +0530

    dfhg

--- CRUD-OP/crude.js
+++ CRUD-OP/crude.js
@@ -8,8 +8,8 @@ document.getElementById("btn").addEventListener("click", function () {
     let gen = document.frm.gen.value;
     let hoby = document.getElementsByName('hobys');
     let hoby2 = [];
-    for(let j=0;j<hoby.length;j++){
-        if(hoby[j].checked==true){
+    for (let j = 0; j < hoby.length; j++) {
+        if (hoby[j].checked == true) {
             hoby2.push(hoby[j].value)
         }
     }
@@ -21,10 +21,10 @@ document.getElementById("btn").addEventListener("click", function () {
         age: age,
         salary: sal,
         gender: gen,
-        hobys:hoby2
+        hobys: hoby2
     };
 
-   
+
 
     let info = JSON.parse(localStorage.getItem("UserInfo"));
 
@@ -37,7 +37,7 @@ document.getElementById("btn").addEventListener("click", function () {
                     info.studentInfo[i].salary = sal;
                     info.studentInfo[i].gender = gen;
                     info.studentInfo[i].hobys = hoby2;
-                   
+
                 }
             }
             localStorage.setItem("UserInfo", JSON.stringify(info));
@@ -56,42 +56,42 @@ document.getElementById("btn").addEventListener("click", function () {
 
     document.frm.reset();
     document.frm.update.value = '';
-     display();
+    display();
 
 
 });
 
 function display() {
 
-    let dt = "<tr>";
-    dt += "<td>Name</td>";
-    dt += "<td>Age</td>";
-    dt += "<td>Salary</td>";
-    dt += "<td>Gender</td>";
-    dt += "<td>Hoby</td>";
-    dt += "<td colspan='2'>Action</td>";
-    dt += "</tr>"; 
-    
+    let dt = "<tr >";
+    dt += "<td align='center'>Name</td>";
+    dt += "<td align='center'>Age</td>";
+    dt += "<td align='center'>Salary</td>";
+    dt += "<td align='center'>Gender</td>";
+    dt += "<td align='center'>Hoby</td>";
+    dt += "<td colspan='2' align='center'>Action</td>";
+    dt += "</tr>";
+
     let info = localStorage.getItem("UserInfo");
     if (info != null) {
         let data = JSON.parse(info);
-   
-            for (let i = 0; i < data.studentInfo.length; i++) {
 
-                dt += "<tr>";
-                dt += "<td>" + data.studentInfo[i].name + "</td>";
-                dt += "<td>" + data.studentInfo[i].age + "</td>";
-                dt += "<td>" + data.studentInfo[i].salary + "</td>";
-                dt += "<td>" + data.studentInfo[i].gender + "</td>";
-                dt += "<td>" + data.studentInfo[i].hobys + "</td>";
-                dt += "<td> <input type='button' name='edit' id='edit' onclick='editdata(" + i + ")' value='Edit'></td>";
-                dt += "<td> <input type='button' name='del' id='del' onclick='deldata(" + i + ")' value='Delete'></td>";
-                dt += "</tr>";
-            } 
-            document.getElementById("userData").innerHTML = dt;
-        
+        for (let i = 0; i < data.studentInfo.length; i++) {
 
-}
+            dt += "<tr>";
+            dt += "<td>" + data.studentInfo[i].name + "</td>";
+            dt += "<td>" + data.studentInfo[i].age + "</td>";
+            dt += "<td>" + data.studentInfo[i].salary + "</td>";
+            dt += "<td>" + data.studentInfo[i].gender + "</td>";
+            dt += "<td>" + data.studentInfo[i].hobys + "</td>";
+            dt += "<td> <input type='button' name='edit' id='edit' onclick='editdata(" + i + ")' value='Edit'></td>";
+            dt += "<td> <input type='button' name='del' id='del' onclick='deldata(" + i + ")' value='Delete'></td>";
+            dt += "</tr>";
+        }
+        document.getElementById("userData").innerHTML = dt;
+
+
+    }
 
 }
 function deldata(id) {
@@ -115,12 +115,12 @@ function editdata(id) {
                 document.frm.age.value = data.studentInfo[i].age;
                 document.frm.salary.value = data.studentInfo[i].salary;
                 document.frm.gen.value = data.studentInfo[i].gender;
-                
-                let h =data.studentInfo[i].hobys;
+
+                let h = data.studentInfo[i].hobys;
                 let h2 = document.getElementsByName('hobys');
 
-                for(let j=0;j<h2.length;j++){
-                    if(h.includes(h2[j].value)){
+                for (let j = 0; j < h2.length; j++) {
+                    if (h.includes(h2[j].value)) {
                         h2[j].checked = true;
                     }
                 }
